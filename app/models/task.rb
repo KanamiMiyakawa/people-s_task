78d8d6f2bdb29dfe111a89075ca34f97cbae6da6
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-06 11:04:24 +0000

    create task views

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
 
   validates :task_name,  presence: true
-  validates :priority,  presence: true, inclusion: {in:1..10}
+  validates :priority,  presence: true, inclusion: {in:1..5}
   validates :status,  presence: true, inclusion: {in:1..3}
 
 end
