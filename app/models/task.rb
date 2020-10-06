8cb3afbe01f0c36a7aeabe7dc2ea26d792c5cd89
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-06 07:23:11 +0000

    add validation on TaskModel

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+
+  validates :task_name,  presence: true
+  validates :priority,  presence: true, inclusion: {in:1..10}
+  validates :status,  presence: true, inclusion: {in:1..3}
+
 end
