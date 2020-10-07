22adba9b4f3f910be0c3f990657aa644b6427e26
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-07 06:03:49 +0000

    update model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
-
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'タスクモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'タスクのタイトルが空の場合' do
+      it 'バリデーションにひっかかる１' do
+        task = Task.new(task_name: '', content: '失敗テスト')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクの詳細が空の場合' do
+      it 'バリデーションにひっかかる２' do
+        task = Task.new(task_name: "失敗", content: 'nil')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      it 'バリデーションが通る' do
+        task = FactoryBot.create(:task)
+        expect(task).to be_valid
+      end
+    end
+  end
 end
