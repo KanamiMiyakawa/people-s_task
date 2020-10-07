74a339731dd8ddf164fde28b0e5c8fbbf2929060
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-07 05:51:28 +0000

    update system spec

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,14 +1,20 @@
 FactoryBot.define do
   # 作成するテストデータの名前を「task」とします
   # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
-  factory :task do
-    task_name { 'Factoryで作ったデフォルトのタイトル１' }
-#    content { 'Factoryで作ったデフォルトのコンテント１' }
-  end
+  # factory :task do
+  #   task_name { 'Factoryで作ったデフォルトのタイトル１' }
+  #   content { 'Factoryで作ったデフォルトのコンテント１' }
+  #   priority {1}
+  #   limit {Date.new(2020,11,11)}
+  #   status {2}
+  # end
   # 作成するテストデータの名前を「second_task」とします
   # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
-  factory :second_task, class: Task do
+  factory :system_task, class: Task do
     task_name { 'Factoryで作ったデフォルトのタイトル２' }
-#    content { 'Factoryで作ったデフォルトのコンテント２' }
+    content { 'Factoryで作ったデフォルトのコンテント２' }
+    priority {2}
+    limit {Date.new(2020,11,11)}
+    status {3}
   end
 end
--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -4,6 +4,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in "タスク名", with: 'task2'
+        fill_in "詳細", with: 'taskcontent2'
+        select '1', from: '優先順位'
+        page.find('#task_limit').set("2021-01-01")
+        choose 'task_status_1'
+        click_on '登録'
+        expect(page).to have_content 'task2'
       end
     end
   end
@@ -11,12 +19,12 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, task_name: 'task')
+        task = FactoryBot.create(:system_task)
         # タスク一覧ページに遷移
         visit tasks_path
         # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
         # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
-        expect(page).to have_content 'task_failure'
+        expect(page).to have_content 'Factoryで作ったデフォルトのタイトル２'
         # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
@@ -24,6 +32,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
+         task = FactoryBot.create(:system_task)
+         visit task_path(task.id)
+         expect(page).to have_content 'Factoryで作ったデフォルトのコンテント２'
        end
      end
   end
