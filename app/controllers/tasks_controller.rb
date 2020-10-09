2c43a2f5461ce34e04930881a88b24900f17ca66
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-09 06:36:52 +0000

    add sort_limit on task#index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,11 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.order(created_at: "DESC")
+    if params[:sort_limit]
+      @tasks = Task.all.order(limit: "DESC")
+    else
+      @tasks = Task.all.order(created_at: "DESC")
+    end
   end
 
   def show
