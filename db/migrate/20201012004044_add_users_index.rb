e6d77658011ff11ae0a5426922339064472be544
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-12 00:53:46 +0000

    create UserModel

---
+++ db/migrate/20201012004044_add_users_index.rb
@@ -0,0 +1,6 @@
+class AddUsersIndex < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :name
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_150924) do
+ActiveRecord::Schema.define(version: 2020_10_12_004900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2020_10_09_150924) do
     t.string "content", null: false
     t.index ["status"], name: "index_tasks_on_status"
     t.index ["task_name"], name: "index_tasks_on_task_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
