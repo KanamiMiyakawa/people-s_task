8b735e063464d88ee4a4c444c5f67d2079cd277f
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-12 11:26:16 +0000

    update admin_users_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,14 @@
 class Admin::UsersController < ApplicationController
+  before_action :admin_user
+
+  def index
+    @users = User.all.order(created_at: "DESC")
+  end
+
+  private
+
+  def admin_ser
+    redirect_to tasks_path unless current_user.admin?
+  end
+
 end
