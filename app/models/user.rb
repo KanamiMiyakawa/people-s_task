21454d000b2a4178e51984918acd5133a5f5e205
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-12 01:20:15 +0000

    add first user data with seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
 
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   has_secure_password
