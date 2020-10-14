9c32e5bc865a355f81753a3d0d5b9837698342b2
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-14 05:49:50 +0000

    generate labels_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   root 'tasks#index'
 
   resources :tasks
-
   resources :users, only: [:new, :create, :show]
-
   namespace :admin do
     resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   end
-
   resources :sessions, only: [:new, :create, :destroy]
+  resources :labels, only: [:new, :create, :destroy]
+  
 end
