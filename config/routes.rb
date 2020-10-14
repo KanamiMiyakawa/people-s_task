93ba4b5e5834c951ea0cc23f75a9a9d09bcaa60f
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-14 06:32:57 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   namespace :admin do
     resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+    resources :labels, only: [:new, :create, :destroy]
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :labels, only: [:new, :create, :destroy]
-  
+
 end
