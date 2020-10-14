899069ad1e2780fe536456c93265b2dec3f2dea4
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-14 05:21:48 +0000

    migrate label

--- db/migrate/20201014051715_create_labels.rb
+++ db/migrate/20201014051715_create_labels.rb
@@ -1,8 +1,8 @@
 class CreateLabels < ActiveRecord::Migration[5.2]
   def change
     create_table :labels do |t|
-      t.string :label_name
-      t.boolean :official
+      t.string :label_name, null: false
+      t.boolean :official, default: false
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_111049) do
+ActiveRecord::Schema.define(version: 2020_10_14_051715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "label_name", null: false
+    t.boolean "official", default: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_labels_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "task_name", null: false
@@ -39,5 +48,6 @@ ActiveRecord::Schema.define(version: 2020_10_12_111049) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "labels", "users"
   add_foreign_key "tasks", "users"
 end
