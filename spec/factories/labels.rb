fe86eba93562bad7519613375f795721beb08132
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-15 15:15:58 +0000

    update label spec

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,12 +1,4 @@
 FactoryBot.define do
-  #official_label_manager作成
-  factory :official_label_manager, class: User do
-    name {"official_label_manager"}
-    email {"official@example.com"}
-    password {"password"}
-    password_confirmation {"password"}
-    admin {true}
-  end
 
   factory :official_label_1, class: Label do
     label_name {"official_label_1"}
--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -87,17 +87,21 @@ RSpec.describe 'ラベリング機能', type: :system do
 
   describe 'タスクのラベル付け機能' do
     before do
+      #ユーザー作成
       @official_label_manager = FactoryBot.create(:official_label_manager)
       @user1 = FactoryBot.create(:user1)
+      #ラベル作成
       @official_label_1 = FactoryBot.create(:official_label_1, user: @official_label_manager)
+      @official_label_2 = FactoryBot.create(:official_label_2, user: @official_label_manager)
       @user_label_1 = FactoryBot.create(:user_label_1, user: @user1)
-      visit new_session_path
+      @user_label_2 = FactoryBot.create(:user_label_2, user: @user1)
       #user1でログイン
+      visit new_session_path
       fill_in "session_email", with: "fac1@example.com"
       fill_in "session_password", with: "password"
       click_on 'login_button'
     end
-    context 'ラベルを付けてタスクを作成' do
+    context 'ラベルを付けてタスクを作成した場合' do
       it 'タスク詳細にラベルが表示されている' do
         click_button 'タスク作成'
         fill_in "タスク名", with: 'labelling_task_1'
@@ -110,7 +114,96 @@ RSpec.describe 'ラベリング機能', type: :system do
         click_on '登録'
         #タスク詳細に遷移
         expect(page).to have_content 'official_label_1' && 'user_label_1'
+      end
+    end
+    context 'ラベル付きのタスクを編集した場合' do
+      it 'タスク詳細に編集前のラベルがなく、編集後のラベルがある' do
+        @task = FactoryBot.create(:index_test1, user: @user1)
+        #ラベルを２つ付与
+        FactoryBot.create(:labelling, task: @task, label: @official_label_1)
+        FactoryBot.create(:labelling, task: @task, label: @user_label_1)
+        visit task_path(@task)
+        #ラベルがついていることを確認
+        expect(page).to have_content 'official_label_1' && 'user_label_1'
+        #さきほどとは別のラベルを付与
+        click_on '編集'
+        check("task_label_ids_#{@official_label_2.id}")
+        check("task_label_ids_#{@user_label_2.id}")
+        click_on '更新する'
+        #さきほどのラベルがない
+        expect(page).to_not have_content 'official_label_1' && 'user_label_1'
+        #編集時に付与したラベルがある
+        expect(page).to have_content 'official_label_2' && 'user_label_2'
+      end
+    end
+  end
 
+  describe 'ラベル検索機能' do
+    before do
+      #ユーザー作成
+      @official_label_manager = FactoryBot.create(:official_label_manager)
+      @user = FactoryBot.create(:user1)
+      #ラベル作成
+      @task_1 = FactoryBot.create(:search_test1, user: @user)
+      @task_2 = FactoryBot.create(:search_test2, user: @user)
+      @task_3 = FactoryBot.create(:search_test3, user: @user)
+      @task_4 = FactoryBot.create(:search_test4, user: @user)
+      @task_5 = FactoryBot.create(:search_test5, user: @user)
+      #ラベル作成
+      @label_1 = FactoryBot.create(:official_label_1, user: @official_label_manager)
+      @label_2 = FactoryBot.create(:user_label_1, user: @user)
+      #ラベリング
+      FactoryBot.create(:labelling, task: @task_1, label: @label_1)
+      FactoryBot.create(:labelling, task: @task_2, label: @label_2)
+      FactoryBot.create(:labelling, task: @task_3, label: @label_2)
+      FactoryBot.create(:labelling, task: @task_4, label: @label_2)
+      #userでログイン
+      visit new_session_path
+      fill_in "session_email", with: "fac1@example.com"
+      fill_in "session_password", with: "password"
+      click_on 'login_button'
+      visit tasks_path
+    end
+    context 'ラベルのみで検索した場合' do
+      it 'タスク一覧に検索したラベル名のみが表示' do
+        #@label_1で検索
+        select "#{@label_1.label_name}", from: 'label_id'
+        click_button '検索'
+        within '.task_tbody' do
+          #@task_1と@label_1がある
+          expect(page).to have_content "#{@task_1.task_name}" && "#{@label_1.label_name}"
+          #@label_2がない
+          expect(page).to_not have_content "#{@label_2.label_name}"
+        end
+      end
+    end
+    context 'ラベルとタイトルで検索した場合' do
+      it 'タスク一覧に検索したラベル、タイトル名のみ表示' do
+        #「ルパン」、@label_2で検索
+        fill_in "title", with: "ルパン"
+        select "#{@label_2.label_name}", from: 'label_id'
+        click_button '検索'
+        within '.task_tbody' do
+          #「ルパン」、@label_2がある
+          expect(page).to have_content "ルパン" && "#{@label_2.label_name}"
+          #他のタイトル、@label_1がない
+          expect(page).to_not have_content "銭形" && "不二子" && "#{@label_1.label_name}"
+        end
+      end
+    end
+    context 'ラベルとタイトルとステータスで検索した場合' do
+      it 'タスク一覧に検索したラベル、タイトル、ステータスのみ表示' do
+        #「銭形」、「着手」、@label_2で検索
+        fill_in "title", with: "銭形"
+        select "#{@label_2.label_name}", from: 'label_id'
+        select '着手', from: 'status'
+        click_button '検索'
+        within '.task_tbody' do
+          #「銭形」、「着手」、@label_2がある
+          expect(page).to have_content "銭形" && "#{@label_2.label_name}" && "着手"
+          #他のタイトル、ステータス、@label_1がない
+          expect(page).to_not have_content "銭形" && "不二子" && "#{@label_1.label_name}" && "未着手" && "完了"
+        end
       end
     end
   end
