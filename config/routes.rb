8cf3eba8a8aff8e1666239f7e6509669960f5a5b
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-15 03:28:55 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'labellings/create'
-  get 'labellings/destroy'
   root 'tasks#index'
 
   resources :tasks
   resources :users, only: [:new, :create, :show]
+  resources :labels, only: [:new, :create, :destroy]
   namespace :admin do
     resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
     resources :labels, only: [:new, :create, :destroy]
   end
   resources :sessions, only: [:new, :create, :destroy]
-  resources :labels, only: [:new, :create, :destroy]
-
+  resources :labellings, only: [:create, :destroy]
 end
