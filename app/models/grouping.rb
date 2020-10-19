b59e7f20a821cd4585e9e824d5d23779b09afcff
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-19 09:38:39 +0000

    update grouping model

--- app/models/grouping.rb
+++ app/models/grouping.rb
@@ -1,4 +1,12 @@
 class Grouping < ApplicationRecord
+  before_destroy :owner_cant_remove
+
   belongs_to :user
   belongs_to :group
+
+  def owner_cant_remove
+    if current_user.id == Group.find(self.group_id).user.id
+      throw :abort
+    end
+  end
 end
