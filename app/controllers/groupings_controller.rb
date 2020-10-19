f2216513722bcbf1b610cda332384aa61d4701b5
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-19 08:12:13 +0000

    create grouping create

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,6 +1,12 @@
 class GroupingsController < ApplicationController
 
   def create
+    groupings = current_user.groupings.new(group_id:params[:group_id])
+    if groupings.save
+      redirect_to groups_path, notice: 'グループに参加しました'
+    else
+      redirect_to groups_path, notice: 'グループに参加できませんでした'
+    end
   end
 
   def destroy
