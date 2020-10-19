92c5fa502fa21fb93a4e50fa64ed35841c2bc01e
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-19 05:56:53 +0000

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_021320) do
+ActiveRecord::Schema.define(version: 2020_10_19_055455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group_name"
+    t.string "group_detail"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "labellings", force: :cascade do |t|
     t.bigint "task_id"
@@ -57,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_021320) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "labellings", "labels"
   add_foreign_key "labellings", "tasks"
   add_foreign_key "labels", "users"
